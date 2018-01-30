d5538c8fc17139ce5a2054ad41da36700d754089
Mantas <mantas.muzzz@gmail.com>
2018-01-30 15:14:27 +0200

    Pabaigiau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Parašykite programą, kuri paprašytų įvesti vartotojo du skaičius ir atspausdinti jų
+        //skirtumą bei sumą
+
+        Scanner in = new Scanner(System.in);
+        int sk1, sk2;
+
+        System.out.println("Iveskite du skaicius: ");
+        sk1 = in.nextInt();
+        sk2 = in.nextInt();
+
+        System.out.println("Suma: " + (sk1 + sk2));
+        System.out.println("Skirtumas: " + (sk1 - sk2));
     }
 }
